80bab8945cfc4053c2ca17e7844c84af4f0e2551
Anna Wang <anna.a.wang@gmail.com>
2017-07-18 11:45:23 -0400

    gulp file structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var fileinclude = require('gulp-file-include');
 
 gulp.task('sass', function() {
-    gulp.src('sass/**/*.scss')
+    gulp.src('src/sass/*.scss')
         .pipe(sass({
             sourceComments: true,
             outputStyle: 'expanded',
             errLogToConsole: true
         }))
-        .pipe(gulp.dest('css/'));
+        .pipe(gulp.dest('dist/css/'));
 });
 
 gulp.task('default', ['sass'], function() {
-    var sassWatcher = gulp.watch('sass/**/*.scss', ['sass']);
+    var sassWatcher = gulp.watch('src/sass/*.scss', ['sass']);
+});
+
+gulp.task('html', function() {
+	return gulp.src(['src/html/pages/*.html'])
+	.pipe(fileinclude({
+		prefix: '@@',
+		basepath: '@file'
+	}))
+	.pipe(gulp.dest('./dist/html/'))
 });
