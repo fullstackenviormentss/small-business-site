ef7d22d3cc01299908de1b98d9f84468009896a9
Anna Wang <anna.a.wang@gmail.com>
2017-07-20 14:01:25 -0400

    fixed scrollspy

--- dist/js/smb.js
+++ dist/js/smb.js
@@ -1,9 +1,9 @@
 $(window).scroll(function() {
-	var fixSidebar = $('.usa-nav').innerHeight() + $('.usa-navbar').innerHeight();
+	var fixSidebar = $('.usa-layout-docs-main_content').offset().top;
 	var contentHeight = $('.usa-layout-docs-main_content').innerHeight();
-	var sidebarHeight = $('.usa-layout-docs-sidenav').height();
+	var sidebarHeight = $('.usa-sidenav-list').innerHeight();
 	var footerHeight = $('footer').height();
-	var sidebarBottomPos = contentHeight - sidebarHeight - footerHeight;
+	var sidebarBottomPos = contentHeight - sidebarHeight;
 	var trigger = $(window).scrollTop() - fixSidebar;
 
 	if ($(window).scrollTop() >= fixSidebar) {
@@ -13,8 +13,70 @@ $(window).scroll(function() {
 	}
 
 	if (trigger >= sidebarBottomPos) {
+		$('.usa-layout-docs-sidenav').addClass('bottom');
 		$('.usa-sidenav-list').addClass('bottom');
 	} else {
 		$('.usa-sidenav-list').removeClass('bottom');
 	}
 });
+
+hash = function(h){
+  if (history.pushState){
+    history.pushState(null, null, h);
+  }else{
+    location.hash = h;
+  }
+};
+
+// scrollspy section
+$(function($){
+  //variable that will hold the href attr of the links in the menu
+  var sections = [];
+  //variable that stores the id of the section
+  var id = false;
+  //variable for the selection of the anchors in the navbar
+  var $navbara = $('.usa-sidenav-list a');
+  //li 
+  var toCurrent = $('.usa-sidenav-list li')
+  
+  $navbara.click(function(e){
+    //prevent the page from refreshing
+    e.preventDefault();
+    //set the top offset animation and speed
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+	},1000);
+    
+    hash($(this).attr('href'));
+  });
+  
+  //select all the anchors in the navbar one after another
+  $navbara.each(function(){
+   // and adds them in the sections variable
+    sections.push($($(this).attr('href')));
+    
+  })
+
+  $(window).scroll(function(e){
+    // scrollTop retains the value of the scroll top with the reference at the middle of the page
+    var scrollTop = $(this).scrollTop() + ($(window).height()/12);
+    //cycle through the values in sections array
+    for (var i in sections) {
+      var section = sections[i];
+      //if scrollTop variable is bigger than the top offset of a section in the sections array then 
+      if (scrollTop > section.offset().top){
+        var scrolled_id = section.attr('id');
+      }
+    }
+    if (scrolled_id !== id) {
+      id = scrolled_id;
+      $($navbara).removeClass('current');
+      $('.usa-sidenav-list a[href="#' + id + '"]').addClass('current'); 
+    }
+  });
+
+
+
+});
+
+
